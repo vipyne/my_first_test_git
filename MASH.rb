13b6bef6ac58cfc60b5625994321af5317a5feec
vanessa <vipyne@gmail.com>
2013-10-16 22:09:08 -0700

    initial MASH.rb

---
+++ MASH.rb
@@ -0,0 +1,22 @@
+
+module Puts
+  def winner
+  	puts "you drive a #{Car.new}"
+  end
+end
+
+class Man_apt_sha_hou
+  def initialize
+
+  end
+
+
+end
+
+
+class Cars
+  def initialize(user = {})
+
+  end
+
+end
